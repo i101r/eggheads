7243e4d8aab7acf4487fed3aca38790b630403a0
Ilya <i@101r.ru>
2023-02-16 20:19:25 -0500

    test

--- CTestFix.php
+++ CTestFix.php
@@ -2,8 +2,8 @@
 require_once($_SERVER['DOCUMENT_ROOT'].'/CTestTask.php');
 
 class CTestFix extends CTestTask{
-	var $db;
-	
+    var $db;
+    
     var $arDb=[
         'host'=>'localhost',
         'user'=>'dev3',
@@ -22,43 +22,43 @@ class CTestFix extends CTestTask{
         "ONLY_FULL_GROUP_BY"=>"SET SESSION sql_mode='ONLY_FULL_GROUP_BY';",
 
     ];
-	
-	var $arTables=[
-		'users'=>[
-			'NAME'=>'users',
-			'PRIMARY_KEY'=>'id',
-			'FIELDS'=>[
-				'id'=>'int(11) NOT NULL AUTO_INCREMENT',
-				'phone'=>'varchar(255)',
-				'email'=>'varchar(255)',
-				'gender'=>'varchar(255)',
-				'name'=>'varchar(255)',
-				'created'=>'date',
-			],
-		],
-		'orders'=>[
-			'PRIMARY_KEY'=>'id',
-			'NAME'=>'orders',
-			'FIELDS'=>[
-				'id'=>'int(11) NOT NULL AUTO_INCREMENT',
-				'subtotal'=>'decimal(11)',
-				'city_id'=>'int(11)',
-				'user_id'=>'int(11)',
-				'created'=>'date',
-			],
-		],
-		'questions'=>[
-			'PRIMARY_KEY'=>'id',
-			'NAME'=>'questions',
-			'FIELDS'=>[
-				'id'=>'int(11) NOT NULL AUTO_INCREMENT',
-				'catalog_id'=>'int(11)',
-				'user_id'=>'int(11)',
-				'text'=>'varchar(255)',
-				'created'=>'date',
-			],
-		]
-	];
+    
+    var $arTables=[
+        'users'=>[
+            'NAME'=>'users',
+            'PRIMARY_KEY'=>'id',
+            'FIELDS'=>[
+                'id'=>'int(11) NOT NULL AUTO_INCREMENT',
+                'phone'=>'varchar(255)',
+                'email'=>'varchar(255)',
+                'gender'=>'varchar(255)',
+                'name'=>'varchar(255)',
+                'created'=>'date',
+            ],
+        ],
+        'orders'=>[
+            'PRIMARY_KEY'=>'id',
+            'NAME'=>'orders',
+            'FIELDS'=>[
+                'id'=>'int(11) NOT NULL AUTO_INCREMENT',
+                'subtotal'=>'decimal(11)',
+                'city_id'=>'int(11)',
+                'user_id'=>'int(11)',
+                'created'=>'date',
+            ],
+        ],
+        'questions'=>[
+            'PRIMARY_KEY'=>'id',
+            'NAME'=>'questions',
+            'FIELDS'=>[
+                'id'=>'int(11) NOT NULL AUTO_INCREMENT',
+                'catalog_id'=>'int(11)',
+                'user_id'=>'int(11)',
+                'text'=>'varchar(255)',
+                'created'=>'date',
+            ],
+        ]
+    ];
 
    
 
@@ -67,66 +67,66 @@ class CTestFix extends CTestTask{
     }
 
     function initDbData(){
-		$this->initTables();
-		$this->initUsers();
-		$this->initOrders();
-		$this->initQuestions();
+        $this->initTables();
+        $this->initUsers();
+        $this->initOrders();
+        $this->initQuestions();
     }
-	
-	function initTables(){
-		foreach($this->arTables as $table){
-			$this->createTable($table);
-		}
-	}
-	
-	function genPhone(){
-		$f='+7';
-		$s=' ('.rand(111,999).') ';
-		$t=rand(111,999).'-'.rand(11,99).'-'.rand(11,99);
-		return $f.$s.$t;
-	}
-	
-	function initUsers(){
-		for($i=1;$i<10;$i++){
-			$this->addUser([
-				'phone'=>$this->genPhone(),
-				'email'=>'user'.$i.'@test.ru',
-				'gender'=>(rand(1,10)%2)?'male':'female',
-				'name'=>'user_'.$i,
-				'created'=>date('Y-m-d H:i:s'),
-				
-			]);
-		}
-	}
-	
-	function initQuestions(){
-		for($i=1;$i<10;$i++){
-			$this->addItem('questions',[
-				'catalog_id'=>rand(1,3),
-				'text'=>'question'.$i,
-				'user_id'=>$i,
-				'created'=>date('Y-m-d H:i:s'),
-				
-			]);
-		}
-	}
-	
-	function initOrders(){
-		$orderid=1;
-		for($i=1;$i<10;$i++){
-			for($j=1;$j<10;$j++){
-				$this->addOrder([
-					// 'id'=>$orderid,
-					'subtotal'=>rand(1000,9999),
-					'city_id'=>rand(1,1000),
-					'user_id'=>$i,
-					'created'=>date('Y-m-d H:i:s'),
-				]);
-				
-				$orderid++;
-			}
-		}
-	}
+    
+    function initTables(){
+        foreach($this->arTables as $table){
+            $this->createTable($table);
+        }
+    }
+    
+    function genPhone(){
+        $f='+7';
+        $s=' ('.rand(111,999).') ';
+        $t=rand(111,999).'-'.rand(11,99).'-'.rand(11,99);
+        return $f.$s.$t;
+    }
+    
+    function initUsers(){
+        for($i=1;$i<10;$i++){
+            $this->addUser([
+                'phone'=>$this->genPhone(),
+                'email'=>'user'.$i.'@test.ru',
+                'gender'=>(rand(1,10)%2)?'male':'female',
+                'name'=>'user_'.$i,
+                'created'=>date('Y-m-d H:i:s'),
+                
+            ]);
+        }
+    }
+    
+    function initQuestions(){
+        for($i=1;$i<10;$i++){
+            $this->addItem('questions',[
+                'catalog_id'=>rand(1,3),
+                'text'=>'question'.$i,
+                'user_id'=>$i,
+                'created'=>date('Y-m-d H:i:s'),
+                
+            ]);
+        }
+    }
+    
+    function initOrders(){
+        $orderid=1;
+        for($i=1;$i<10;$i++){
+            for($j=1;$j<10;$j++){
+                $this->addOrder([
+                    // 'id'=>$orderid,
+                    'subtotal'=>rand(1000,9999),
+                    'city_id'=>rand(1,1000),
+                    'user_id'=>$i,
+                    'created'=>date('Y-m-d H:i:s'),
+                ]);
+                
+                $orderid++;
+            }
+        }
+    }
 
     function createTable($arTable){
         
@@ -136,67 +136,67 @@ class CTestFix extends CTestTask{
         }
 
         $arTable['FIELDS']=implode(',',$arFields);
-		
+        
         $q=$this->getQuery('CREATE_TABLE',$arTable);
 
         $this->query($q);
     }
-		
-	function addOrder($arOrder){
-		$this->addItem('orders',$arOrder);
-	}
-	
-	function addUser($arUser){
-		$this->addItem('users',$arUser);
-	}
-	
-	function addItem($table,$arItem){
-		$table=$table;
-		foreach($this->arTables[$table]['FIELDS'] as $fkey=>$field){
-			$arFields[$fkey]=$fkey;
-			$arValues[$fkey]=$arItem[$fkey]?'"'.$arItem[$fkey].'"':'NULL';
-		}
-		
-		$arQuery=[
-			"TABLE_NAME"=>$table,
-			"FIELDS"=>implode(',',$arFields),
-			"VALUES"=>implode(',',$arValues)
-		];
-		
-		$q=$this->getQuery('INSERT',$arQuery);
-		$this->query($q);
-		
-	}
-	
+        
+    function addOrder($arOrder){
+        $this->addItem('orders',$arOrder);
+    }
+    
+    function addUser($arUser){
+        $this->addItem('users',$arUser);
+    }
+    
+    function addItem($table,$arItem){
+        $table=$table;
+        foreach($this->arTables[$table]['FIELDS'] as $fkey=>$field){
+            $arFields[$fkey]=$fkey;
+            $arValues[$fkey]=$arItem[$fkey]?'"'.$arItem[$fkey].'"':'NULL';
+        }
+        
+        $arQuery=[
+            "TABLE_NAME"=>$table,
+            "FIELDS"=>implode(',',$arFields),
+            "VALUES"=>implode(',',$arValues)
+        ];
+        
+        $q=$this->getQuery('INSERT',$arQuery);
+        $this->query($q);
+        
+    }
+    
     function dbConn(){
         $this->db= new mysqli(
-			$this->arDb['host'], 
-			$this->arDb['user'], 
-			$this->arDb['passwd'], 
-			$this->arDb['dbname']
-		);
-		
-		if ($this->db->connect_error) {
-			die('Connect Error (' . $this->db->connect_errno . ') ' . $this->db->connect_error);
-		}
-		
-		$this->query($this->getQuery('ONLY_FULL_GROUP_BY'));
-		
+            $this->arDb['host'], 
+            $this->arDb['user'], 
+            $this->arDb['passwd'], 
+            $this->arDb['dbname']
+        );
+        
+        if ($this->db->connect_error) {
+            die('Connect Error (' . $this->db->connect_errno . ') ' . $this->db->connect_error);
+        }
+        
+        $this->query($this->getQuery('ONLY_FULL_GROUP_BY'));
+        
     }
 
     function query($query,$obj=false){
         $res=$this->db->query($query);
    
-		if (gettype ($res)=='object'){
-			if($obj) return $res;
+        if (gettype ($res)=='object'){
+            if($obj) return $res;
 
-			while($row=$res->fetch_assoc()){
-				$arRows[]=$row;
-			}
-		}
-		else{
-			return $res;
-		}
+            while($row=$res->fetch_assoc()){
+                $arRows[]=$row;
+            }
+        }
+        else{
+            return $res;
+        }
 
         if(is_array($arRows) && count($arRows)==1) return $arRows[0];
         
@@ -204,12 +204,12 @@ class CTestFix extends CTestTask{
     }
 
     function getQuery($temp,$arVars=[]){
-		$q=$this->arQueriesTemp[$temp];
-		
-		foreach($arVars as $key=>$var){
-			$q=str_replace("#$key#",$var,$q);
-		}
-		
+        $q=$this->arQueriesTemp[$temp];
+        
+        foreach($arVars as $key=>$var){
+            $q=str_replace("#$key#",$var,$q);
+        }
+        
         return $q;
     }
 
@@ -226,19 +226,19 @@ class CTestFix extends CTestTask{
     }
 
     function getQuestionsByCat2($catId){
-		
-		$catId=intval($catId);
+        
+        $catId=intval($catId);
 
         if(!$catId) return false;
 
         $q=$this->getQuery('GET_QUESTIONS_USER',["ID"=>$catId]);
-		
-		$arQuestions= $this->query($q);
-		
-		return $arQuestions;
-		
-	}
-	
+        
+        $arQuestions= $this->query($q);
+        
+        return $arQuestions;
+        
+    }
+    
     function getQuestionsByCat($catId){
 
         $catId=intval($catId);
@@ -249,8 +249,8 @@ class CTestFix extends CTestTask{
         
         $oReq= $this->query($q,true);
 
-		if(!$oReq) return false;
-		
+        if(!$oReq) return false;
+        
         while($arQuestion=$oReq->fetch_assoc()) {
             $q=$this->getQuery('GET_USER2',["ID"=>$arQuestion['user_id']]);
             
@@ -267,7 +267,7 @@ class CTestFix extends CTestTask{
     }
 
     function getUserBillInfo($uid){
-		$q=$this->getQuery('USER_BILL_INFO',["ID"=>$uid]);
+        $q=$this->getQuery('USER_BILL_INFO',["ID"=>$uid]);
         $arUser=$this->query($q);
         return $arUser;
     }
--- index.php
+++ index.php
@@ -4,53 +4,53 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/CTestFix.php');
 $arMessage=[];
 
 switch($_REQUEST['action']) {
-	case 'InitDataBase':
-		(new CTestFix)->initDbData();
-		$arMessage[]='Database Init';
-		break;
-	case 'getUser':
-		$arUser=(new CTestFix)->getUser();
-		if($arUser){
-			$arMessage[]='User: '.implode(' ,',$arUser);
-		}
-		else{
-			$arMessage[]='User: not found';
-		}
-		break;
-	case 'getQuestionsByCat':
-		$catid=intval($_REQUEST['id']);
-		
-		$arQuestions=(new CTestFix)->getQuestionsByCat2($catid);
-		
-		if($arQuestions){
-			$arMessage[]='Questions: ';
-			foreach($arQuestions as $arQuestion){
-				if($arQuestion['question']){
-					$arQuestion=array_merge($arQuestion['question'],$arQuestion['user']);
-				}
-				
-				$arMessage[]=implode(', ',$arQuestion);
-				
-			}
-		}
-		else{
-			$arMessage[]='Questions: not found';
-		}
-		break;
-	case 'getUserBillInfo':
-		$userid=intval($_REQUEST['id']);
-		$arUserBillInfo=(new CTestFix)->getUserBillInfo($userid);
-		
-		if($arUserBillInfo){
-			
-			$arMessage[]=implode(', ',$arUserBillInfo);
-		}
-		else{
-			$arMessage[]='User Bills: not found';
-		}
-		
-		break;
-}	
+    case 'InitDataBase':
+        (new CTestFix)->initDbData();
+        $arMessage[]='Database Init';
+        break;
+    case 'getUser':
+        $arUser=(new CTestFix)->getUser();
+        if($arUser){
+            $arMessage[]='User: '.implode(' ,',$arUser);
+        }
+        else{
+            $arMessage[]='User: not found';
+        }
+        break;
+    case 'getQuestionsByCat':
+        $catid=intval($_REQUEST['id']);
+        
+        $arQuestions=(new CTestFix)->getQuestionsByCat2($catid);
+        
+        if($arQuestions){
+            $arMessage[]='Questions: ';
+            foreach($arQuestions as $arQuestion){
+                if($arQuestion['question']){
+                    $arQuestion=array_merge($arQuestion['question'],$arQuestion['user']);
+                }
+                
+                $arMessage[]=implode(', ',$arQuestion);
+                
+            }
+        }
+        else{
+            $arMessage[]='Questions: not found';
+        }
+        break;
+    case 'getUserBillInfo':
+        $userid=intval($_REQUEST['id']);
+        $arUserBillInfo=(new CTestFix)->getUserBillInfo($userid);
+        
+        if($arUserBillInfo){
+            
+            $arMessage[]=implode(', ',$arUserBillInfo);
+        }
+        else{
+            $arMessage[]='User Bills: not found';
+        }
+        
+        break;
+}    
 
 ?>
 <!DOCTYPE html>
@@ -58,28 +58,28 @@ switch($_REQUEST['action']) {
     <head>
         <meta charset="utf-8">
         <script async="" src="js/script.js"></script>
-		<style>
-			.msgs{
-				width:500px;
-				height:300px;
-				padding:30px;
-			}
-		</style>
+        <style>
+            .msgs{
+                width:500px;
+                height:300px;
+                padding:30px;
+            }
+        </style>
     </head>
     <body>
-		<form action="/" method="GET">
-			<input type="text" name="id" value="<?=$_REQUEST['id']?$_REQUEST['id']:'1'?>">
-			<input type="button" id="user-bill-info" value="User Bill Info" >  
-			<input type="submit" name="action" value="InitDataBase" >  
-			<input type="submit" name="action" value="getUser" >  
-			<input type="submit" name="action" value="getQuestionsByCat" >  
-			<input type="submit" name="action" value="getUserBillInfo" >  
-		</form>
-		<div class="msgs">
-			<?foreach($arMessage as $msg):?>
-				<div class="msg"><?=$msg;?></div>
-			<?endforeach;?>	
-		</div>
+        <form action="/" method="GET">
+            <input type="text" name="id" value="<?=$_REQUEST['id']?$_REQUEST['id']:'1'?>">
+            <input type="button" id="user-bill-info" value="User Bill Info" >  
+            <input type="submit" name="action" value="InitDataBase" >  
+            <input type="submit" name="action" value="getUser" >  
+            <input type="submit" name="action" value="getQuestionsByCat" >  
+            <input type="submit" name="action" value="getUserBillInfo" >  
+        </form>
+        <div class="msgs">
+            <?foreach($arMessage as $msg):?>
+                <div class="msg"><?=$msg;?></div>
+            <?endforeach;?>    
+        </div>
 
     </body>
 </html>
